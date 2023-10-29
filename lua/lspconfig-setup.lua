fff9802f7b7789fa9948fcd94e54b6e6da8af8c6
Johann-Peter Duchon <dejaypiii@gmail.com>
2023-10-29 09:51:17 +0100

    Update and add go lsp

--- lua/lspconfig-setup.lua
+++ lua/lspconfig-setup.lua
@@ -9,6 +9,7 @@ end
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 lspconfig.clangd.setup({})
 lspconfig.cmake.setup({})
+lspconfig.gopls.setup({})
 lspconfig.lua_ls.setup({
     on_attach = on_attach,
     settings = {
